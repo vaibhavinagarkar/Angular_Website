ca65d8f1977287e745a218ad653c4f40ea2ca400
vaibhavinagarkar <vaibhavin24@.com>
2021-07-25 21:48:51 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturesComponent } from './features/features.component';
+import { DocsComponent } from './docs/docs.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { EventsComponent } from './events/events.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TextComponent } from './text/text.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: TextComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'docs', component: DocsComponent },
+  { path: 'blogs', component:  BlogsComponent },
+  { path: 'resources', component: ResourcesComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeaturesComponent } from './features/features.component';
+import { DocsComponent } from './docs/docs.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { EventsComponent } from './events/events.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    DocsComponent,
+    ResourcesComponent,
+    EventsComponent,
+    BlogsComponent,
+    TextComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
